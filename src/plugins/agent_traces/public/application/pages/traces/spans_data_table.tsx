b1c13fbc48c04416c3d120f6c79c5fc9dd082c23
Joshua Li <joshuali925@gmail.com>
2026-03-11 03:00:35 +0000

    fix(agent-traces): make horizontal scrollbar always visible in data tables
    Signed-off-by: Joshua Li <joshuali925@gmail.com>

--- src/plugins/agent_traces/public/application/pages/traces/spans_data_table.tsx
+++ src/plugins/agent_traces/public/application/pages/traces/spans_data_table.tsx
@@ -264,7 +264,7 @@ export const SpansDataTable: React.FC = () => {
 
   return (
     <TraceExpansionProvider value={expansionContextValue}>
-      <div className="agentTracesTable__container">
+      <div className="agentTracesTable__container eui-xScrollWithShadows">
         <EuiFlexGroup alignItems="center" justifyContent="spaceBetween" gutterSize="m">
           <EuiFlexItem grow={false}>
             <EuiText size="s">

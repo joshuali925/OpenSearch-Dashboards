99a87575ce9841c56214b8be481895b99fdb9c91
Joshua Li <joshuali925@gmail.com>
2026-03-12 16:44:58 +0000

    fixup! Refactor traces and spans tables to use DataTable component

--- src/plugins/agent_traces/public/components/fields_selector/field_list.tsx
+++ src/plugins/agent_traces/public/components/fields_selector/field_list.tsx
@@ -9,7 +9,7 @@ import { DataViewField } from '../../../../data/public';
 import { DiscoverField } from './discover_field';
 import { DiscoverSidebarProps } from './discover_sidebar';
 import { FieldDetails } from './types';
-import { AGENT_TRACES_DEFAULT_COLUMNS_SET } from '../../../common';
+import { AGENT_TRACES_DEFAULT_COLUMNS } from '../../../common';
 
 interface FieldGroupProps extends DiscoverSidebarProps {
   category: 'query' | 'discovered' | 'selected';
